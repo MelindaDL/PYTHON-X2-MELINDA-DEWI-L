d544aa7340ff1e5c7e94bb931f12821c06290217
MelindaDL <@melindadewi193@gmail.com>
2021-02-24 13:37:25 +0700

    Program

---
+++ Perulangan/Program06.2.py
@@ -0,0 +1,9 @@
+kata = 'nama saya melinda dewi listiyani'
+
+jum = 0
+for letter in kata :
+    if letter == 'd':
+        jum += 1
+        #continued
+    #print ('huruf sekarang : ', letter)
+print ('jumlah : ', jum)
